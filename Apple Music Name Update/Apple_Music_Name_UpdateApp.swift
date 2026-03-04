43a31732b826237b16173fe1788ab31020326c72
edwardzhu <edward.zhu93@gmail.com>
2026-03-03 23:57:07 -0800

    Initial Commit

---
+++ Apple Music Name Update/Apple_Music_Name_UpdateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Music_Name_UpdateApp.swift
+//  Apple Music Name Update
+//
+//  Created by 祝嘉栋 on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Music_Name_UpdateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
